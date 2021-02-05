9ac415b194c36ef65ba22ed915376c86ea91489a
dev-towfiq <towfiq.alahe@northsouth.edu>
2021-02-05 23:10:23 +0600

    first commit

---
+++ problems.py
@@ -0,0 +1,4 @@
+import random
+
+num=random.randint(0,100) #Generate a random number between 0 to 100
+print(num)
---
+++ start.py
@@ -0,0 +1,55 @@
+print("Towfiq")
+age=15
+print(age)
+
+string1="This is my string"
+print(string1)
+
+firstName="Justin"
+lastName="Timberlake"
+
+print(firstName+" "+lastName)
+
+#Comment
+#User Input
+
+# name=input("Input your name:")
+# print(name)
+
+# num1=input("Number 1:")
+# num2=input("Number 2:")
+
+# n1=int(num1)
+# n2=int(num2)
+
+#print(n1+n2)
+
+#conditional statement
+price=500
+if price>500:
+    print("Price is Greater than 500")
+else:
+    print("Price is not grater than 500")
+
+#List
+ages=[13,15,45,96,74]
+
+#for loop
+for age in ages:
+    print("Age:",age)
+
+#for loop range
+for num in range (0,10):
+    print(num)
+
+#While loop
+num=0
+while num<10:
+        print(num)
+        num=num+1
+
+#Define Function
+def brushTeeth():
+    print("Brushing....")    
+#Call Function
+brushTeeth()

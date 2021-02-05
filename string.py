0ccad5e35e7fafce3c859dcec0aca6d342701c9a
dev-towfiq <towfiq.alahe@northsouth.edu>
2021-02-06 00:15:20 +0600

    string

---
+++ string.py
@@ -0,0 +1,25 @@
+course="Python for Beginners"
+print(course)
+print(course.upper())
+
+print(course.find('y')) #return the index of 'y'
+print(course.find('for')) #return the index of 'for'
+
+print(course.replace('for','4')) #replace string or sub-string
+
+print('Python' in course) #Return Boolean value 
+
+print(10**3) #10^3 
+
+print(10/3) #return decimal value
+print(10//3) #return integer value
+print(10%3) #return reminder
+
+#equation
+x=10+3*2
+print(x) # x=10+3*2 = 16
+x=(10+3)*2
+print(x) # x=(10+3)*2 = 26
+
+  
+
